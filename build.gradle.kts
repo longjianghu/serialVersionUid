593cee72c2d2c76f3bc4e72d35b9be5cda43a76f
longjianghu <215241062@qq.com>
2025-03-13 22:08:27 +0800

    feat:调整快捷键

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2024.1.7")
+    version.set("2023.3.3")
     type.set("IU")
 
     plugins.set(listOf(
