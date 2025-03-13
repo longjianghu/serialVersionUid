2ba8f6622be8b6376ff0bfe0f9f2a354fffa81df
longjianghu <215241062@qq.com>
2025-03-13 22:21:37 +0800

    feat:调整插件配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.sohocn"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -34,7 +34,7 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("241")
+        sinceBuild.set("233")
         untilBuild.set("243.*")
     }
 
